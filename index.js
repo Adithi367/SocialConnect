b61049bdcc81a44c38144f7d78902311655915d0
R Adithi Shenoy <adishenoy4@gmail.com>
2026-04-23 11:16:02 +0530

    updat

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoConnection()
 app.use(cookieParser())
 app.use(cors({
     //origin:['http://localhost:5173'],
-    origin:['https://social-connect-qjig9we0q-adithi367s-projects.vercel.app'],
+    origin:['https://social-connect-qjig9we0q-adithi367s-projects.vercel.app','https://social-connect-axca4c2kc-adithi367s-projects.vercel.app'],
     
     credentials:true
 }))
